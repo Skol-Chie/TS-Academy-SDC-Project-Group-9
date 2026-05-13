0d1827c6e9ad6bd0f29476d87ceeddf778b23374
harkinlaby <akinlabibamidele@gmail.com>
2026-05-13 13:00:23 +0100

    created navbar-feature

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "../assets/planet-logo.png"; 
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={logo} alt="//" />
+      </div>
+    
+        
+    </nav>
+  );
+};
+
+export default Navbar;
