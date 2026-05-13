872a1a7df05225af1a98e7c93f28996138c9cf50
West <promisedentonwest@gmail.com>
2026-05-13 13:53:13 +0100

    feat: connecting the CTA button to the planet image and the contact form

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -4,7 +4,7 @@ import planets from "../assets/images/planets.jpeg";
 
 export default function PlanetCard({ planet, distanceFromSun, image, }) {
   return (
-    <div className="planet-card">
+    <div className="planet-card" id="planets">
       <img src={planets} alt={planet} className="planet-image" />
       <h2 className="planet-name">{planet}</h2>
       <p>Distance from Sun</p>
